2f19f1a38979d6d73451d41d7dd73a83856396cb
chaudarybilal <chaudarybilal618@gmail.com>
2023-06-15 16:24:41 +0500

    create pages and connect with react router dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home"
+import About from "./components/About"
+import Blog from "./components/Blog"
+import Contact from "./components/Contact"
+import Project from "./components/Project"
+
+
+
 
 const App = () => {
+
   return (
-    <div>App</div>
+    <>
+<BrowserRouter>
+  <Routes>
+    <Route path ="/" element={<Home/>}/>
+    <Route path ="about" element={<About/>}/>
+    <Route path ="/blog" element={<Blog/>}/>
+    <Route path ="/contact" element={<Contact/>}/>
+    <Route path ="/project" element={<Project/>}/>
+  </Routes>
+</BrowserRouter>
+
+    </>
   )
 }
 
