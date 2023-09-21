adc32d86e268988beb2ab56467de235fa442a689
chaudarybilal <chaudarybilal618@gmail.com>
2023-09-21 23:09:03 +0500

    add project data

--- src/Footer.js
+++ src/Footer.js
@@ -4,35 +4,35 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <>
-      <div
-        style={{
-          height: "20vh",
-          width: "100%",
-          position: "relative",
-          backgroundColor: "white",
-        }}
-      >
-        <div style={{ position: "absolute", left: "50%", top: "25%" }}>
-          <div
-            style={{
-              width: "80px",
-              display: "flex",
-              justifyContent: "space-around",
-            }}
-          >
-            <GitHubIcon style={{ fontSize: 30 }} />
-            <LinkedInIcon style={{ fontSize: 30 }} />
-          </div>
+    <div
+      style={{
+        height: "20vh",
+        width: "100%",
+        position: "relative",
+        backgroundColor: "white",
+      }}
+    >
+      <div style={{ position: "absolute", left: "50%", top: "50%", transform: "translate(-50%, -50%)" ,display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"center"}}>
+        <div
+          style={{
+            width: "80px",
+            display: "flex",
+            alignItems:"center",
+            justifyContent: "space-around",
+            marginBottom: "10px", // Spacing between icons and text
+          }}
+        >
+          <GitHubIcon style={{ fontSize: 30 }} />
+          <LinkedInIcon style={{ fontSize: 30 }} />
         </div>
-        <div style={{ position: "absolute", left: "47%", top: "50%" }}>
-          <div>@Muhammad Bilal</div>
+        <div style={{ textAlign: "center" }}>
+          @Muhammad Bilal
         </div>
-        <div style={{ position: "absolute", left: "42%", top: "70%" }}>
+        <div style={{ textAlign: "center", fontSize: "0.8rem" }}>
           Icons used from Flaticon & Font Awesome
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,71 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Container, Grid, Card, CardContent, Link } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(4),
+  },
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+}));
+
+const projects = [
+  {
+    id: 1,
+    title: 'Giphyengine',
+    description: 'Our Giphy search engine website is a vibrant and entertaining platform dedicated to the world of animated GIFs. Designed to provide users with quick and easy access to an extensive library of GIFs, our website offers a delightful and immersive experience for both casual users and GIF enthusiasts',
+    githubLink: 'https://github.com/chaudarybilal/Giphyengine',
+  },
+  {
+    id: 2,
+    title: 'E-Commerce',
+    description: 'Our platform is dedicated to bringing you a world of convenience, choice, and quality right at your fingertips. Discover a wide range of products and services curated to cater to your every need and desire.',
+    githubLink: 'https://github.com/chaudarybilal/ecom',
+  },
+  {
+    id: 3,
+    title: 'Live-Weather',
+    description: 'Stay ahead of the elements with our cutting-edge Live Weather App, your go-to companion for real-time weather information and forecasts. Whether you are planning your day, a weekend getaway or a week-long vacation, our app provides you with up-to-the-minute weather updates and essential meteorological insights to help you make informed decisions.',
+    githubLink: 'https://github.com/chaudarybilal/-Live-Weather',
+  },
+];
 
 const Project = () => {
+  const classes = useStyles();
 
   return (
-    <div>Project</div>
-  )
-}
+    <Container className={classes.root}>
+      <Typography variant="h2" component="h1" align="center">
+        Projects
+      </Typography>
+      <Grid container spacing={3}>
+        {projects.map((project) => (
+          <Grid item xs={12} sm={6} md={4} key={project.id}>
+            <Card className={classes.card}>
+              <CardContent className={classes.cardContent}>
+                <Typography variant="h5" component="h2">
+                  {project.title}
+                </Typography>
+                <Typography variant="body1" style={{ marginBottom: '16px' }}>
+                  {project.description}
+                </Typography>
+                <Link href={project.githubLink} target="_blank" rel="noopener">
+                  GitHub Repository
+                </Link>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
 
-export default Project
+export default Project;
